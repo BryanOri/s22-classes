9871f50b93abedc3c622f83d253c3a7e620edff2
BryanOri <bryanorihuela@computingforall.org>
2022-01-15 13:42:48 -0800

    stuff

--- classes.js
+++ classes.js
@@ -1,9 +1,42 @@
 // Step 1: Create a base class called ZooAnimal that can store the animal name, animal type, and adoption date. It can also print these details to the console when needed.
 
-
+class ZooAnimal{
+    static adopted = 0;
+    constructor(animalName, animalType, adoptionDate){
+        this.animalName = animalName; 
+        this.animalType = animalType;
+        this.adoptionDate =  adoptionDate;
+        ZooAnimal.adopted++;
+    }
+    printDetails(){
+        console.log(this.animalName, this.animalType, this.adoptionDate);
+    }
+    static printAmount(){
+        console.log(ZooAnimal.adopted, `total amount of animals adopted`);
+    }
+}
 
 // Step 2: Create extended classes off ZooAnimal called ZooBird and ZooMammal that can automatically store the animal type without passing it in. You will need to create constructors for ZooBird and ZooMammal that only take the animal name and adoption date, and then use the "super" keyword inside them to utilize the ZooAnimal constructor to store the data.
-
+class ZooBird extends ZooAnimal{
+    static birdAdopted = 0;
+    constructor(animalName, adoptionDate){
+        super(animalName, `Bird`, adoptionDate);
+        ZooBird.birdAdopted++
+    }
+    static printAmount(){
+        console.log(ZooBird.birdAdopted, `is the bird amount adopted`)
+    }    
+}
+class ZooMammal extends ZooAnimal{
+    static mammalAdopted = 0;
+    constructor(animalName, adoptionDate){
+        super(animalName, `Mammal`, adoptionDate);
+        ZooMammal.mammalAdopted++;
+    }
+    static printAmount(){
+        console.log(ZooMammal.mammalAdopted, `is the mammal amount adopted`)
+    }
+}
 
 
 // Step 3: Modify ZooAnimal to use a static variable in its constructor to track how many Zoo Animals have been adopted, and update that variable any time a new Zoo Animal is created. Also use a static method to add the ability to print this info to the console when needed. Because the "super" keyword was used earlier to utilize the ZooAnimal constructor, the static variable will be updated automatically.
